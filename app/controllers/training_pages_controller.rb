ad1f300077a14dfb7623a7e1084560b0c73d6d5d
thepaut <p-thepaut@ankh-systems.co.jp>
2018-04-04 17:59:42 +0900

    add get question data from db

--- app/controllers/training_pages_controller.rb
+++ app/controllers/training_pages_controller.rb
@@ -1,5 +1,10 @@
 class TrainingPagesController < ApplicationController
   def new
-    @question = "Coucou?"
+    @question = Question.joins(:categorie).select("questions.text, categories.label as cat, good_answer, bad_answer").first    
+    bad_answer = @question.bad_answer.split(",").map!{ |s| s.to_i}    #split string into array of integer for sql request
+    good_answer = @question.good_answer.split(",").map!{ |s| s.to_i}  #split string into array of integer for sql request
+    @response_number = good_answer.size
+    @answers = Answer.find(good_answer << bad_answer)
+    @answers.shuffle!
   end
 end
