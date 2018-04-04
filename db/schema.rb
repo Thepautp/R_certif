e22f4c46a72d4431f46066c5bd7c37ef314eea63
thepaut <p-thepaut@ankh-systems.co.jp>
2018-04-04 16:30:49 +0900

    add answer table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180404071235) do
+ActiveRecord::Schema.define(version: 20180404073006) do
+
+  create_table "answers", force: :cascade do |t|
+    t.string "text"
+    t.string "reason"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "categories", force: :cascade do |t|
     t.string "label"
