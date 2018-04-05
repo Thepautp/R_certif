375bfc5e692729459083d1706ed531624ad7e961
thepaut <p-thepaut@ankh-systems.co.jp>
2018-04-05 15:02:45 +0900

    add result page

--- app/controllers/training_pages_controller.rb
+++ app/controllers/training_pages_controller.rb
@@ -1,6 +1,6 @@
 class TrainingPagesController < ApplicationController
   def new
-    @question = Question.joins(:categorie).select("questions.text, categories.label as cat, good_answer, bad_answer").first    
+    @question = Question.joins(:categorie).select("questions.id, questions.text, categories.label as cat, good_answer, bad_answer").first    
     bad_answer = @question.bad_answer.split(",").map!{ |s| s.to_i}    #split string into array of integer for sql request
     good_answer = @question.good_answer.split(",").map!{ |s| s.to_i}  #split string into array of integer for sql request
     @response_number = good_answer.size
@@ -11,7 +11,19 @@ class TrainingPagesController < ApplicationController
   def result
     if params[:response]
       response = params[:response][:answer_id].split(",")
-      puts response      
-    end
+      user_answer = response[0]
+      question_id = params[:response][:question_id]
+      @question = Question.find(question_id)                           #asked question
+      good_answer_ids = @question.good_answer.split(",").map!{|s| s}  #array with good_answer ids
+      bad_answer_ids = @question.bad_answer.split(",").map!{|s| s}    #array with bad_answer ids
+      @bad_answers = Answer.find(bad_answer_ids)
+      @good_answers = Answer.find(good_answer_ids)                    #params is an array for always having array on output
+      if good_answer_ids.size == 1
+        @is_good_answer = false
+        @is_good_answer = true if response[0] == good_answer_ids[0]
+      else
+        # TODO: multiple_response case
+      end
+    end    
   end
 end
