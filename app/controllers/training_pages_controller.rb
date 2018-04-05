008ef59ba17ef0e51f807e50063932de742335b5
thepaut <p-thepaut@ankh-systems.co.jp>
2018-04-05 15:45:21 +0900

    fix sql query

--- app/controllers/training_pages_controller.rb
+++ app/controllers/training_pages_controller.rb
@@ -1,6 +1,7 @@
 class TrainingPagesController < ApplicationController
   def new
-    @question = Question.joins(:categorie).select("questions.id, questions.text, categories.label as cat, good_answer, bad_answer").first    
+    all_question = Question.joins(:categorie).select("questions.id, questions.text, categories.label as cat, good_answer, bad_answer").all
+    @question = all_question[rand(all_question.size)]
     bad_answer = @question.bad_answer.split(",").map!{ |s| s.to_i}    #split string into array of integer for sql request
     good_answer = @question.good_answer.split(",").map!{ |s| s.to_i}  #split string into array of integer for sql request
     @response_number = good_answer.size
