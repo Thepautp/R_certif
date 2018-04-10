a5b24e4d528a08194b38b404c941345ab9f1f004
thepaut <p-thepaut@ankh-systems.co.jp>
2018-04-10 18:29:34 +0900

    add question controller and co

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,4 @@
+class QuestionsController < ApplicationController
+  def delete
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "training", to: "training_pages#new"
   get "admin", to: "training_pages#add_question"
@@ -7,5 +8,6 @@ Rails.application.routes.draw do
   post "admin/question", to: "training_pages#question_manage"
   get "admin/ajax", to: "training_pages#ajax_api"
   post "result", to: "training_pages#result"
+  get 'questions/delete'
   root "training_pages#new"
 end
