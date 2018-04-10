d68e12c20b40e8410bec4e99939ce86b61aa57af
thepaut <p-thepaut@ankh-systems.co.jp>
2018-04-10 17:42:09 +0900

    refactoring js

---
+++ public/js/training.js
@@ -0,0 +1,90 @@
+$(document).ready(function() {
+  $(".add_field").click(function(){
+    addField(this);
+  });
+  
+  $(".del_field").click(function(){
+    delField(this);
+  });
+  
+  $("#question_update_question").change(recoverData());
+});
+
+function addField(elem){
+  var numberOfField = $(".good_field").length;
+  if (numberOfField < 3) {
+    $(elem).before('<input class="good_field" type="text" name="add_question[good_answer_'+ (numberOfField + 1) +']" id="add_question_good_answer_'+ (numberOfField + 1) +'">');
+    $(".good_reason").last().after('<input class="good_reason" type="text" name="add_question[good_reason_'+ (numberOfField + 1) +']" id="add_question_good_reason_'+ (numberOfField + 1) +'">');
+  }
+  numberOfField = $(".good_field").length;
+  if (numberOfField == 3){
+    $(".add_field").addClass("hidden_field");
+  }
+  var numberOfDelBtn = $(".del_field").length;
+  if (numberOfField > 1 && numberOfField <= 3 && numberOfDelBtn > 0) {
+    $(".del_field").removeClass("hidden_field");
+  }
+}
+
+function delField(elem){
+  var numberOfField = $(".good_field").length;
+  if (numberOfField > 1){
+    $("#add_question_good_answer_"+numberOfField).remove();
+    $("#add_question_good_reason_"+numberOfField).remove();
+    numberOfField = $(".good_field").length;
+  }
+  if (numberOfField == 1){
+    $(".del_field").addClass("hidden_field");
+  }
+  if (numberOfField < 3){
+    $(".add_field").removeClass("hidden_field");
+  }
+}
+
+function recoverData() {
+  var question_id = $(this).val();
+  $.ajax({
+    url: "/admin/ajax",
+    method: "GET",
+    dataType: "json",
+    data: {question_id: question_id},
+    error: function (xhr, status, error) {
+      console.error('AJAX Error: ' + status + error);
+    },
+    success: function (response) {
+      console.log(response);
+      var good_answers = response["good"];
+      var wrong_answers = response["wrong"];
+      var snippet = response["snippet"];
+      if ( snippet == null){
+        snippet = "";
+      }
+      $(".good_answer_part").empty();
+      $(".wrong_answer_part").empty();
+      $(".rank_part").empty();
+      $(".snippet_part").empty();
+      for(var i=0; i< good_answers.length; i++){
+         $(".good_answer_part").append('<div class="answer_container good_container ga_'+i+'"></div>');
+         $('.ga_'+i+'').append('<div class="left_part ">Good answer '+(i+1)+':</div>');
+         $('.ga_'+i+'').append('<div class="right_part rp_ga_'+i+'"></div>');
+         $('.rp_ga_'+i+'').append('<input type="text" name="good_answer_text[]" value="' + good_answers[i]["text"] + '">');
+         $('.rp_ga_'+i+'').append('<input type="text" name="good_answer_reason[]" value="' + good_answers[i]["reason"] + '">');
+       }
+       for(var i=0; i< wrong_answers.length; i++){
+         $(".wrong_answer_part").append('<div class="answer_container wa_'+i+'"></div>') ;
+         $('.wa_'+i+'').append('<div class="left_part ">Wrong answer '+(i+1)+':</div>');
+         $('.wa_'+i+'').append('<div class="right_part rp_wa_'+i+'"></div>');
+         $('.rp_wa_'+i+'').append('<input type="text" name="wrong_answer_text[]" value="' + wrong_answers[i]["text"] + '">');
+         $('.rp_wa_'+i+'').append('<input type="text" name="wrong_answer_reason[]" value="' + wrong_answers[i]["reason"] + '">');
+       }
+       if(response["rank"] == 1 ){
+         $(".rank_part").append('<select name="question_update[level]" id="question_update_level"><option value="0">silver</option><option value="1" selected="selected">gold</option></select>');
+       }else{
+         $(".rank_part").append('<select name="question_update[level]" id="question_update_level"><option value="0" selected="selected">silver</option><option value="1">gold</option></select>');
+       }
+       $(".snippet_part").append('<div class="left_part ">Snippet: </div>');
+       $(".snippet_part").append('<div class="right_part rp_snippet"></div>');
+       $(".rp_snippet").append('<textarea class="question_field" name="snippet" id="add_question_text">'+ snippet +'</textarea>');
+    }
+  });//-end ajax
+}
