5835650e64995c34f3e4d6177fc2ca1ab904a8f6
thepaut <p-thepaut@ankh-systems.co.jp>
2018-04-13 15:42:09 +0900

    add more seeds

--- db/question_seeds.rb
+++ db/question_seeds.rb
@@ -1,8 +1,17 @@
 questions_list = [
   [8,"Which of the following are NOT reserved words in Ruby?",0,
     [["try","Is not a reserved word"],["goto","Is not a reserved word"]],
-    [["super","Is a reserved word"],["begin","Is a reserved word"],["alias","Is a reserved word"]]  
-  ]  
+    [["super","Is a reserved word"],["begin","Is a reserved word"],["alias","Is a reserved word"]]
+  ],
+  [1,"Which of the following is the correct output for the program given below?",0,
+    [["1","a is defined and will be returned"]],
+    [["nil",""],["An error has occurred",""],["[1,2,3,4]",""],["[1]",""]],
+    "def foo (a,  *b) p  a end foo(1,2,3,4)"
+  ],
+  [9,"Which of the following is the correct output for the program given below?",0,
+    [["2","Return the number of key/pairs"]],
+    [["15",""],["6",""],["4",""]]
+  ]
 ]
 questions_list.each do |q|
   question = Question.create(categorie_id: q[0], text: q[1], rank: q[2], snippet: q[5])
