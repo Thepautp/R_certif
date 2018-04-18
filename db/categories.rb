adb18309d13542fa3042d360edd0ac7b6ed316ef
thepaut <p-thepaut@ankh-systems.co.jp>
2018-04-18 17:28:07 +0900

    fix seeds

--- db/categories.rb
+++ db/categories.rb
@@ -3,7 +3,8 @@ categories = [
   "Hash", "Symbol", "Block", "Iterator", "I/O", "Variable", "Constant",
   "String", "Number", "Range", "Module", "Inheritance", "Mixin", "Operator",
   "Encoding", "Alias", "Exception", "Conditional", "Kernel", "Comment",
-  "Command line", "Pre-defined", "Non local exit", "Lambda", "Proc", "Thread"
+  "Command line", "Pre-defined", "Non local exit", "Lambda", "Proc", "Thread",
+  "Time"
 ]
 categories.each do |cat|
   Categorie.create( label: cat)
