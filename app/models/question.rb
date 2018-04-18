29cfaafcab167e4412eabef754d40b6c6bb5ef1c
thepaut <p-thepaut@ankh-systems.co.jp>
2018-04-18 17:48:16 +0900

    fix destroy

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,5 +1,5 @@
 class Question < ApplicationRecord
   belongs_to :categorie
-  has_many :good_answers
-  has_many :bad_answers
+  has_many :good_answers, dependent: :destroy
+  has_many :bad_answers, dependent: :destroy
 end
