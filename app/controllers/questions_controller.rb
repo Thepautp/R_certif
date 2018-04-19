758496b0d70768e3dea556f05051e8bc6f25b149
thepaut <p-thepaut@ankh-systems.co.jp>
2018-04-19 13:48:11 +0900

    dix delete function

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,7 +1,7 @@
 class QuestionsController < ApplicationController
   def destroy
-    unless params[:question_update][:question].empty?
-      question = Question.find(params[:question_update][:question].to_i)
+    unless params[:delete][:question].empty?
+      question = Question.find(params[:delete][:question].to_i)
       question.destroy
     end
     redirect_to admin_question_path
